57d5c4af4c66d5bb61641554accb5de911e1303f
saishamanth4-design <saishamanth4@gmail.com>
2025-11-19 09:08:50 +0530

    Add files via upload

---
+++ p4d.c
@@ -0,0 +1,13 @@
+  function returning pointer to array element
+#include<stdio.h>
+int* display(int a[],int index)
+{
+    return &a[index];
+}
+int main()
+{
+    int arr[5]={50,10,40,30,20};
+    int*p =display(arr,3);
+    printf("%d",*p);
+    return 0;
+}
